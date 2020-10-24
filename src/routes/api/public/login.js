aba179717a678c00524c21a4e42682778a2ba5c6
Marco D'Agostini <madacol10@gmail.com>
2020-10-23 21:37:35 -0500

    session: added user's permissions

--- src/routes/api/public/login.js
+++ src/routes/api/public/login.js
@@ -7,12 +7,24 @@ export async function post(req, res){
     let user;
     {
         const result = await query(
-            `SELECT user_id, users.name, password_hash, array_agg(roles.name) as roles
+            `SELECT
+                    user_id,
+                    users.name,
+                    password_hash,
+                    ARRAY_REMOVE(ARRAY_AGG(role_id), NULL) roles,
+                    array_merge_agg(permissions) permissions
                 FROM users
+                LEFT JOIN join_users_roles USING (user_id)
                 LEFT JOIN (
-                    join_users_roles
-                    JOIN roles USING ("role_id")
-                ) USING ("user_id")
+                    SELECT
+                        role_id,
+                        roles.name,
+                        ARRAY_AGG(permission_id) permissions
+                    FROM roles
+                    JOIN join_roles_permissions USING (role_id)
+                    JOIN permissions USING (permission_id)
+                    GROUP BY role_id
+                ) roles USING (role_id)
                 WHERE username=$1
                 GROUP BY "user_id"`,
             [username]
@@ -27,8 +39,8 @@ export async function post(req, res){
     }
 
     { // Save user in session, and a cookie will be set in the client
-        const {user_id, name, roles} = user;
-        req.session.user = {user_id, name, roles, username};
+        delete user.password_hash; // Do not include *password_hash* in the session
+        req.session.user = user;
     }
 
     res.json({
