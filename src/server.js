0e886cae4fa6537932d11ed3761e5695f0a2c642
Marco D'Agostini <madacol10@gmail.com>
2020-10-24 20:00:52 -0500

    Added logger to see requests in terminal output
    # Conflicts:
    #	package.json

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import sirv from 'sirv';
 import polka from 'polka';
 import * as sapper from '@sapper/server';
 import sessions from './middlewares/sessions';
+import morgan from 'morgan';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 export default polka() // You can also use Express
 	.use(
+		morgan(dev ? 'dev' : 'combined'),
 		sirv('static', { dev }),
 		sessions(dev),
 		sapper.middleware()
