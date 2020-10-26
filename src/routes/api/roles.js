9250f5ca315dc17265b4246f8b5f2ac3fbcf5c93
Marco D'Agostini <madacol10@gmail.com>
2020-10-25 23:11:52 -0500

    Added endpoint to create roles

--- src/routes/api/roles.js
+++ src/routes/api/roles.js
@@ -1,9 +1,28 @@
 import { compose } from "compose-middleware";
+import { ROLES_CREATE } from "../../constants/PERMISSIONS";
+import { query } from "../../db";
 import checkPermissionsMW from "../../middlewares/checkPermissionsMW";
 
+// Create role
 export const post = compose(
-    checkPermissionsMW(1,2),
-    (req, res) => {
-        res.json({success: 'Yeah!, usuario tiene los permisos con ids 1 y 2 para ver este mensaje!'})
+    checkPermissionsMW(ROLES_CREATE),
+    async (req, res) => {
+        const {name, permissions} = req.body;
+        if (!name) return res.json({error: "Debe enviar un nombre válido"})
+
+        const {rows: [{role_id}]} = await query(
+            `WITH inserted_roles AS (INSERT INTO roles (name) VALUES ($1) RETURNING role_id)
+            INSERT INTO join_roles_permissions (role_id, permission_id)
+            SELECT role_id, permission_id
+            FROM inserted_roles
+            CROSS JOIN unnest($2::int[]) permission_id
+            RETURNING role_id;`,
+            [name, permissions]
+        );
+
+        res.json({
+            success: 'Rol creado exitosamente',
+            data: {role_id}
+        })
     }
 )
