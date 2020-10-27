822cc22ac691601c0fef5765e7d7be4166793181
Mauro D'Agostini <mauroalejandro20@gmail.com>
2020-10-26 20:07:07 -0500

    Making Nueva ODT and setting up RadioButtons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,21 @@
+<style>
+
+	.RadioButtons{
+		flex-flow: column wrap;
+	}
+
+</style>
+
 <script>
-	import Select from '../components/Select.svelte';
+	import 'carbon-components-svelte/css/white.css';
+	//import Select from '../components/Select.svelte';
+	import { TextInput } from "carbon-components-svelte";
+	import { TextArea } from "carbon-components-svelte";
+	import {
+    FormGroup,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";
 	let items = [
 	{value: 'dolar', label: 'Dólares $'},
 	{value: 'bolivar', label: 'Bolívares Bs'},
@@ -9,4 +25,39 @@
 ];
 </script>
 
-<Select {items}/>
+<TextInput labelText="Monto de contrato" placeholder="Ingrese el monto del contrato..." />
+
+<FormGroup legendText="Moneda">
+	<RadioButtonGroup selected="dolar">
+		<RadioButton labelText="Dólares $" value="dolar" />
+		<RadioButton labelText="Bolívares Bs" value="bolivar" />
+		<RadioButton labelText="Pesos COP" value="peso" />
+		<RadioButton labelText="Euros €" value="euro" />
+		<RadioButton labelText="Bitcoin BTC" value="bitcoin" />
+	</RadioButtonGroup>
+</FormGroup>
+
+<FormGroup legendText="Cuenta">
+	<RadioButtonGroup selected="cajachica">
+			<RadioButton labelText="Caja Chica" value="cajachica" />
+			<RadioButton labelText="Chase" value="chase" />
+			<RadioButton labelText="BOD" value="bod" />
+			<RadioButton labelText="Venezuela" value="venezuela" />
+			<RadioButton labelText="Provincial" value="provincial" />
+			<RadioButton labelText="Banesco" value="banesco" />
+			<RadioButton labelText="Mercantil" value="mercantil" />
+			<RadioButton labelText="Caja Chica" value="ajachica" />
+			<RadioButton labelText="Chase" value="hase" />
+			<RadioButton labelText="BOD" value="od" />
+			<RadioButton labelText="Venezuela" value="enezuela" />
+			<RadioButton labelText="Provincial" value="rovincial" />
+			<RadioButton labelText="Banesco" value="anesco" />
+			<RadioButton labelText="Mercantil" value="ercantil" />
+	</RadioButtonGroup>
+</FormGroup>
+
+<TextInput labelText="Cliente" placeholder="Ingrese el cliente..." />
+
+<TextArea labelText="Descripción" placeholder="Ingrese la descripción del trabajo..." />
+
+<!-- <Select {items}/> -->
