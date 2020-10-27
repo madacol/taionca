madacol/taionca

commit 3b101bacd9203caf75ee1c96462caa1eafc8ba51

When a request is not authorized, inform which permissions are required
I had to include permission's name when building `PERMISSION.js` file
I also changed some variables because they now have changed meaning
  by containing the name besides the id

src/middlewares/checkPermissionsMW.js
@@ -3,19 +3,19 @@
 /**
  * Validate if `userPermissions` contains all `permissionsRequired`
  *
- * @param {number[]} permissionsRequired - List of `permission_id` to be validated
+ * @param {number[]} permissions_ids - List of `permission_id` to be validated
  * @param {number[]} userPermissions - List of `permission_id` the user has
  *
  * @return {boolean}
  */
-const checkPermissions = (permissionsRequired, userPermissions) => {
-    return permissionsRequired.every(permissionRequired => {  // All permissionsRequired must pass validation
-        return userPermissions.includes(permissionRequired);
+const checkPermissions = (permissions_ids, userPermissions) => {
+    return permissions_ids.every(permission_id => {  // All *permissions_ids* must pass validation
+        return userPermissions.includes(permission_id);
     })
 }
 
 /**
- * @param {number[]} permissionsRequired
+ * @param {[string, number][]} permissionsRequired
  * @param {{session: {user: {permissions: number[]}}}} req
  * @param {*} res
  * @param {*} next
@@ -23,13 +23,19 @@ const checkPermissions = (permissionsRequired, userPermissions) => {
 const checkPermissions_semiMW = async (permissionsRequired, req, res, next) => {
     const {permissions: userPermissions} = req.session.user;
 
-    const isAuthorized = checkPermissions(permissionsRequired, userPermissions);
+    let permissions_names=[], permissions_ids=[];
+    permissionsRequired.forEach( permission => {
+        const [name, id] = permission;
+        permissions_names.push(name);
+        permissions_ids.push(id);
+    })
+
+    const isAuthorized = checkPermissions(permissions_ids, userPermissions);
     if (isAuthorized) return next();
 
     res.statusCode=403;
     res.json({
-        error: "No está autorizado para realizar esta acción",
-        log: permissionsRequired
+        error: `No está autorizado para realizar esta acción.\n Necesita los siguientes permisos: ${permissions_names.join(', ')}`,
     })
 }
 
@@ -46,3 +52,11 @@ function checkPermissionsMW (...permissionsRequired) {
 }
 
 export default checkPermissionsMW;
+
+
+/**
+ * Los menús y permisos ahora se declaran en el archivo `/contatrib-erp/utils/checkPermissions.js`
+ *
+ * **utils** es un módulo donde se pondrán todas las funciones y clases que se usen en el **back** y en el **front**,
+ * ya que es accesible desde ambos.
+ */

src/scripts/build_permissions_constants.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 (async () => {
     const {rows: permissions} = await query("SELECT permission_id, name FROM permissions");
 
-    const constants_js_declarations = permissions.map( ({permission_id, name}) => (`export const ${name.toUpperCase()} = ${permission_id};`) );
+    const constants_js_declarations = permissions.map( ({permission_id, name}) => (`export const ${name.toUpperCase()} = ['${name}', ${permission_id}];`) );
 
     const file_content = constants_js_declarations.join("\n");
     const filepath = "./src/constants/PERMISSIONS.js";

commit 1591577baaed802796cb904152fb7bed55939a79

Fix type declaration of checkPermissionMW for permissionRequired
- This was broken in commit 3b101bacd9203caf75ee1c96462caa1eafc8ba51
  Didn't noticed until activating `checkJs`

src/middlewares/checkPermissionsMW.js
@@ -40,7 +40,7 @@ const checkPermissions_semiMW = async (permissionsRequired, req, res, next) => {
 /**
  * Pass as arguments all permissions you need to validate
  *
- * @param  {...number} permissionsRequired - List of `permission_id` to be validated
+ * @param  {...(string | number)[]} permissionsRequired - List of `permission_id` to be validated
  *
  * @return {import("polka").Middleware}
  */