89dcf60eab77a612dbf0ef2adc7c0d9fcd311ec6
Mauro D'Agostini <mauroalejandro20@gmail.com>
2021-03-04 20:33:31 -0500

    Last change broke cellphone view (Not Responsive) SOLVED

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,9 +6,9 @@
 <style>
 	main {
 		position: relative;
-		/* max-width: 56em; */
+		max-width: 56em;
 		background-color: white;
-		padding: 2em 10em;
+		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
