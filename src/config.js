60f5bd2ecb7865b358a2cfc03c32151810a7a4db
Marco D'Agostini <madacol10@gmail.com>
2021-04-07 19:21:33 +0200

    Fix: cookie was not set when secure and behind a proxy
    Squashed commit of the following:
    
    commit 9c1d8a523333bd1c3b0f99599c87be6f41c03e76
    Author: Marco D'Agostini <madacol10@gmail.com>
    Date:   Wed Apr 7 19:11:16 2021 +0200
    
        proxy true
    
    commit 4e4a344949cf9c2b55322ea7e0c1d236365113da
    Author: Marco D'Agostini <madacol10@gmail.com>
    Date:   Wed Apr 7 18:49:35 2021 +0200
    
        express 2
    
    commit 6166083b34b1ff0b5e2828df27be98abc7f5bd2d
    Author: Marco D'Agostini <madacol10@gmail.com>
    Date:   Wed Apr 7 18:40:57 2021 +0200
    
        express
    
    commit 54cb89c1583e6c3f11d17bcbcbb058b715159473
    Author: Marco D'Agostini <madacol10@gmail.com>
    Date:   Wed Apr 7 18:14:33 2021 +0200
    
        samesite true
    
    commit 9ec2a79a9885177cc8976f72ca98b6cd242b70bb
    Author: Marco D'Agostini <madacol10@gmail.com>
    Date:   Wed Apr 7 17:36:28 2021 +0200
    
        disable secure
    
    commit ce890e6212b7c319832705112cb1196c47096057
    Author: Marco D'Agostini <madacol10@gmail.com>
    Date:   Wed Apr 7 17:19:25 2021 +0200
    
        test session

--- src/config.js
+++ src/config.js
@@ -12,6 +12,9 @@ export const database = {
   get client(){return this.common},
 }
 
+/**
+ * https://www.npmjs.com/package/express-session#options
+ */
 export const session_options = {
   secret: process.env.SECRET_SESSION, // Execute in bash "openssl rand -base64 30" to get a strong secret string
   resave: false,
@@ -19,8 +22,10 @@ export const session_options = {
   cookie: {
     maxAge: 43200000, // 12 Hours
     httpOnly: true,
+    sameSite: true // Strict
     //domain: "example.com",
   },
+  proxy: true,
   rolling: true,  // Cookie's expiration time will be refreshed on each request
   saveUninitialized: false,
 }
