54cb89c1583e6c3f11d17bcbcbb058b715159473
Marco D'Agostini <madacol10@gmail.com>
2021-04-07 18:14:33 +0200

    samesite true

--- src/config.js
+++ src/config.js
@@ -12,6 +12,9 @@ export const database = {
   get client(){return this.common},
 }
 
+/**
+ * https://www.npmjs.com/package/express-session#options
+ */
 export const session_options = {
   secret: process.env.SECRET_SESSION, // Execute in bash "openssl rand -base64 30" to get a strong secret string
   resave: false,
@@ -19,6 +22,7 @@ export const session_options = {
   cookie: {
     maxAge: 43200000, // 12 Hours
     httpOnly: true,
+    sameSite: true // Strict
     //domain: "example.com",
   },
   rolling: true,  // Cookie's expiration time will be refreshed on each request
--- src/middlewares/sessions.js
+++ src/middlewares/sessions.js
@@ -8,7 +8,7 @@ session_options.store = new PgStore({pool});
 export default dev => {
     dev
         ? session_options.secret = "TRYLr8U0iPhkmzHO+HT+xHkdKovUMBxw0Q4LZp9F"
-        : session_options.cookie.secure = false
+        : session_options.cookie.secure = true
 
     return session(session_options);
 }
