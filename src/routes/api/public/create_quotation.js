c1ff1d21e89a12fa704c63701c22a6e353be80fb
Mauro D'Agostini <mauroalejandro20@gmail.com>
2021-05-28 01:51:42 -0500

    create_quotation.js optimized

--- src/routes/api/public/create_quotation.js
+++ src/routes/api/public/create_quotation.js
@@ -11,21 +11,11 @@ export const post =
         const { general_expenses, inv_expenses, currency, id_client } = req.body;
         const admin_percent = 0.2;
         const rate_usd = await getRate(currency.code, 'usd');
-        const rate_ves = await getRate(currency.code, 'ves');
-        const rate_cop = await getRate(currency.code, 'cop');
-        const rate_eur = await getRate(currency.code, 'eur');
-        const rate_btc = await getRate(currency.code, 'btc');
         const usd_currency = 1;
         const ves_currency = 2;
         const cop_currency = 3;
         const eur_currency = 4;
         const btc_currency = 5;
-        console.log("rate_usd:",rate_usd);
-        console.log("rate_ves:",rate_ves);
-        console.log("rate_cop:",rate_cop);
-        console.log("rate_eur:",rate_eur);
-        console.log("rate_btc:",rate_btc);
-        console.log(general_expenses, inv_expenses, currency, id_client);
 
         if (id_client){
             const {rows: odts} = await sql`
@@ -50,28 +40,26 @@ export const post =
         } else{
             let total_general_expenses = 0; //Same currency of quotation
             for (let i = 0; i < general_expenses.length; i++) {
-                if (general_expenses[i].currency.id_currency === currency.id_currency){
-                    
+                if (general_expenses[i].currency.id_currency === currency.id_currency){ 
                     total_general_expenses += Number(general_expenses[i].amount);
 
                 }else if (general_expenses[i].currency.id_currency === usd_currency && general_expenses[i].currency.id_currency !== currency.id_currency){
-
                     total_general_expenses += Number(general_expenses[i].amount) * rate_usd;
 
                 }else if (general_expenses[i].currency.id_currency === ves_currency && general_expenses[i].currency.id_currency !== currency.id_currency){
-
+                    const rate_ves = await getRate(currency.code, 'ves');
                     total_general_expenses += Number(general_expenses[i].amount) * rate_ves;
 
                 }else if (general_expenses[i].currency.id_currency === cop_currency && general_expenses[i].currency.id_currency !== currency.id_currency){
-
+                    const rate_cop = await getRate(currency.code, 'cop');
                     total_general_expenses += Number(general_expenses[i].amount) * rate_cop;
 
                 }else if (general_expenses[i].currency.id_currency === eur_currency && general_expenses[i].currency.id_currency !== currency.id_currency){
-
+                    const rate_eur = await getRate(currency.code, 'eur');
                     total_general_expenses += Number(general_expenses[i].amount) * rate_eur;
 
                 }else if (general_expenses[i].currency.id_currency === btc_currency && general_expenses[i].currency.id_currency !== currency.id_currency){
-
+                    const rate_btc = await getRate(currency.code, 'btc');
                     total_general_expenses += Number(general_expenses[i].amount) * rate_btc;
 
                 }
@@ -86,9 +74,6 @@ export const post =
             total_inv_expenses = total_inv_expenses * rate_usd;
 
             let contract_amount = (total_general_expenses + total_inv_expenses) / (1-admin_percent);
-
-            console.log("total_general_expenses:",total_general_expenses);
-            console.log("total_inv_expenses:",total_inv_expenses);
         
             res.json({
                 success: `Cotización calculada`,
