389247de3a09e9ec6151c4c6af348d5345441f8e
Marco D'Agostini <madacol10@gmail.com>
2022-08-02 13:33:32 +0200

    Include to logs the response status and message

--- src/middlewares/logs.js
+++ src/middlewares/logs.js
@@ -1,4 +1,4 @@
-export default (req, res, next)=>{
+export default async (req, res, next)=>{
     const body = {...req.body};
         delete body.password
     const path = req.originalUrl;
@@ -7,14 +7,21 @@ export default (req, res, next)=>{
     const headers = req.headers;
     const user_session = req.session.user;
 
-    res.sql`
-    
+    const result = await res.sql`
         INSERT INTO public.logs
         ( body, path, method, params, headers, user_session )
         VALUES (${body}, ${path}, ${method}, ${params}, ${headers}, ${user_session})
         RETURNING id_log;
-    
     `
+    const id_log = result.rows[0].id_log;
+    res.on("finish", ()=>{
+        res.sql`
+            UPDATE public.logs
+            SET response_status_code = ${res.statusCode},
+                response_status_message = ${res.statusMessage}
+            WHERE id_log = ${id_log};
+        `
+    })
     next();
 }
 
